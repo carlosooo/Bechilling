28c5670d7fe2e8a5d4aa4c4485742cce6a86dea2
carlosooo <141675462+carlosooo@users.noreply.github.com>
2023-09-19 19:30:32 -0300

    Add files via upload

--- respiracoes.js
+++ respiracoes.js
@@ -1,26 +1,37 @@
+const timerContainer = document.querySelector("#timer");
+const messageContainer = document.querySelector("#timerMessage")
 
-
-
-
-const timerContainer  = document.querySelector("#timer")
-
-let time = 7
-timerContainer.textContent = time
-
-
-function sla(){
-    if (time < 1){
-        time = 0
-        timerContainer.textContent = time
-
-    }else{
- 
-    timerContainer.textContent = time--
-    setTimeout(sla, 1000)
+function* sla(time) {
+    while (time >= 0) {
+        timerContainer.textContent = time;
+        yield time;
+        yield new Promise(resolve => {
+            setTimeout(resolve, 1000);
+        });
+        time--;
     }
-
 }
 
+const times = {
+    "Inspire": 4,
+    "Segure": 7,
+    "Expire": 8
+}
 
+const botao = document.querySelector("#inicioBotao");
+botao.addEventListener("click", async (e) => {
 
-
+    for (let index in times) {
+        const timerGenerator = sla(times[index]);
+        for (let remainingTime of timerGenerator) {
+            if (typeof remainingTime === 'number') {
+                // Update the timer display
+                timerContainer.textContent = remainingTime ;
+                messageContainer.textContent = index
+            } else if (remainingTime instanceof Promise) {
+                // Wait for 1 second before the next iteration
+                await remainingTime;
+            }
+        }
+    }
+});
